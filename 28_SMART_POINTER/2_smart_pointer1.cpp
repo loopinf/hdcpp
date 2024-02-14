7e97e10aa56b1d76d64125d914c80587f41bc414
loopinf <loopinf@gmail.com>
2024-02-14 14:15:41 +0900

    temp

--- 28_SMART_POINTER/2_smart_pointer1.cpp
+++ 28_SMART_POINTER/2_smart_pointer1.cpp
@@ -8,13 +8,22 @@ public:
 };
 
 // 아래 코드가 핵심
+// 개념: 임의차
+// 원리: -> 와 * 연산자를 overloading 해서 포인터처럼 보이게 하는 것
+// 장점: 객체 임, raw pointer가 아님
+// 		생성/복사/대입/파괴의 모든 과정에서 각각 약속된 함수가 호출됨. 
+//      추가적인 작업이 가능
+// 		대표적인 활용: 소멸자에서 "자원 삭제(delete)" 기능
 class Ptr
 {
 	Car* obj;
 public:
 	Ptr(Car* p = nullptr) : obj(p) {}
+	~Ptr(){ delete obj; }
 
 	Car* operator->() { return obj; }
+	// Car operator*() { return *obj; }  // 값을 반환하면 *obj 를 복사한 임시객체 반환
+	Car& operator*() { return *obj; }  // 임시객체 반환하지 말고, *obj 자체를 반환
 };
 int main()
 {
@@ -25,6 +34,8 @@ int main()
 
 	// Ptr의 객체인 p를 Car* 처럼 씀
 	p->Go(); 
+
+	(*p).Go(); // 
 }
 
 
