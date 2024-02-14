ace8a81d6abddcc12aecacea10a9a36e57000f3f
loopinf <loopinf@gmail.com>
2024-02-14 16:44:21 +0900

    temp

--- 28_SMART_POINTER/6_deleter2.cpp
+++ 28_SMART_POINTER/6_deleter2.cpp
@@ -3,7 +3,15 @@
 
 int main()
 {
-    std::unique_ptr<int> up1(new int); //ok
-    std::unique_ptr<int> up2(new int[10]); //??
+    std::unique_ptr<int> up1(new int);     // ok
+    std::unique_ptr<int> up2(new int[10]); //?? bug 할당은 array new, 소멸자는 array delete 사용안함
 
+    // 해결책 #1. 함수객체 전달
+    std::unique_ptr<int, ArrayDeleter> up2(new int[10]);
+
+    // 해결책 #2. C++14부터 전달
+    std::unique_ptr<int[]> up2(new int[10]);
+
+    // shared_ptr 은 C++17 부터 [] 지원
+    std::shared_ptr<int[]> sp2(new int[10]);
 }
