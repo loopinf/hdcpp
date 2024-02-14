1bc7bd89f0fed0b814b4c112bbeb4de27124c1bd
loopinf <loopinf@gmail.com>
2024-02-14 15:19:51 +0900

    Day4 3PM

--- 28_SMART_POINTER/3_shared_ptr2.cpp
+++ 28_SMART_POINTER/3_shared_ptr2.cpp
@@ -18,7 +18,28 @@ struct Point
 int main()
 {
 	// 아래 코드는 동적 메모리 할당을 몇번하게 될까요 ?
-	std::shared_ptr<Point> sp(new Point(1, 2));
+	// std::shared_ptr<Point> sp(new Point(1, 2));
+	std::shared_ptr<Point> sp = std::make_shared<Point>(1, 2); // memory concat..
+								// 1. sizeof(Point) + sizeof(control_block)할당
+								// 2. Point memory (1,2) 로 초기화
+								// 3. 해당 메모리를 관리하는 shared_ptr만들어서 반환
+								
+	// std::make_shared 의 장점
+	// 1. memory managed efficiently
+	// 2. safety 
+
+	// 아래 코드는 : (2)-(1)-(3) 순서대로 실행되면 안전!
+	//				(2)-(3)-(1) 순서대로 실행되고, (3)인 goo()에서 예외가 발생하면
+	// 							메모리 누수
+	foo( std::shared_ptr<int>(new int), goo());
+	//              (1)          (2)     (3)
+	// 핵심: 자원 할당과, 자원관리객체의 생성은 한번에 해야함.
+				// std::make_shared의 철학
+
+	foo( std::make_shared<int>(0),  // "메모리할당 + shared_ptr"생성, 0은 초기값
+		goo());
+
+
 	
 	
 }
--- 28_SMART_POINTER/3_shared_ptr3.cpp
+++ 28_SMART_POINTER/3_shared_ptr3.cpp
@@ -1,5 +1,6 @@
-#include "helper.h"
+#include <iostream>
 #include <memory>
+#include <string>
 
 struct People
 {
@@ -13,8 +14,14 @@ struct People
 
 int main()
 {
+	// std::make_shared 가 좋지만, 설명을 위해서 new People
 	std::shared_ptr<People> sp1{new People{"kim"}};
 	std::shared_ptr<People> sp2{new People{"lee"}};
+
+	// 아래코드, 메모리 누수 (p204 diagram)
+	// 상호참조, 원형 참조(cycle reference) 발생
+	sp1->bf = sp2;
+	sp2->bf = sp1;
 
 }
 
