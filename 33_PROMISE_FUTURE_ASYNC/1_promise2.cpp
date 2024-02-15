3ba88bd056a465bef1b7a0b70eeabd9d643536d5
loopinf <loopinf@gmail.com>
2024-02-15 14:39:48 +0900

    .

--- 33_PROMISE_FUTURE_ASYNC/1_promise2.cpp
+++ 33_PROMISE_FUTURE_ASYNC/1_promise2.cpp
@@ -8,11 +8,20 @@ using namespace std::literals;
 // => thread 로 수행하는 함수에서 결과값을 얻을 때 사용
 
 // 1. thread 함수는 "promise" 객체를 참조로 받아야함
-int add(std::promise<int>& p, int a, int b)
+int add(std::promise<int> &p, int a, int b)
 {
-	int s = a + b;
-	// 2. 연산이 종료되면 p.set_value로 알려줌
-	p.set_value(s);
+	try
+	{
+		int s = a + b;
+		// 2. 연산이 종료되면 p.set_value로 알려줌
+		p.set_value(s);
+	}
+	catch (const std::exception &e)
+	{
+		// 결과 뿐만 아니라, 예외도 전달 가능
+		std::cerr << e.what() << '\n';
+		p.set_exception(std::current_exception());
+	}
 }
 int main()
 {
@@ -21,5 +30,14 @@ int main()
 	std::promise<int> p;
 	std::future<int> ft = p.get_future();
 	// 2. 스레드 생성할 때 promise를 참조로 전달
-	std::thread t(&add, std::ref(p), 1,2);
+	std::thread t(&add, std::ref(p), 1, 2);
+
+	// 3. main thread 는 다른 작업 하다가
+
+	// 4. 결과가 필요하면, futures를 꺼내면 됨
+	int ret = ft.get(); // 결과가 있으면 바로 꺼냄
+						// 결과가 없으면, p.set_value 할때까지 대기
+						// ft.wait(시간)으로 특정시간 대기도 가능
+
+	std::cout <<
 }
