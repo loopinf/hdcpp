4f9c4402b73d92571db50e05252909e06bfb304a
loopinf <loopinf@gmail.com>
2024-02-15 15:22:29 +0900

    .

--- 34_MUTEX_LOCK/1_mutex1.cpp
+++ 34_MUTEX_LOCK/1_mutex1.cpp
@@ -7,11 +7,15 @@ using namespace std::literals;
 
 void delay() { std::this_thread::sleep_for(20ms); }
 
+std::mutex m;  //스레드간 동기화 도구
+
 void foo(std::string_view name)
 {
     // stack         : 스레드가 소유
     // static memory : process 가 소유
     // heap memory   : process 가 소유
+    // GUI handle    : 스레드가 소유
+
     // int x = 0;    // 지역변수는 thread당 하나임
     //                 // 2개가 수행되면, 각각 사용
 
@@ -19,14 +23,21 @@ void foo(std::string_view name)
 
     for (int i = 0; i < 10; i++)
     {
+        m.lock();
+        // ------------------------------------
         x = 100;   delay();
         x = x + 1; delay();
         std::cout << name << " : " << x << std::endl; delay();
+        // ------------------------------------
+        m.unlock();
     }
 }
 
 int main()
 {
+    // 2개 스레드 내려가다가... m.lock 만남. 
+    // 대기. 
+    // 직렬화 라고 함
     std::thread t1(foo, "A");
     std::thread t2(foo, "\tB");
     t1.join();
