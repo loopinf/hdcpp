6877ecf2ce943bad2170c8fdb76deba6122d19ca
loopinf <loopinf@gmail.com>
2024-02-15 15:24:09 +0900

    .

--- 34_MUTEX_LOCK/1_mutex2.cpp
+++ 34_MUTEX_LOCK/1_mutex2.cpp
@@ -4,22 +4,23 @@
 #include <mutex>
 using namespace std::literals;
 
-
+std::mutex m;
 int share_data = 0;
 
 void foo()
 {
+    m.lock();
     share_data = 100;
+    m.unlock();
 }
 
 int main()
 {
     std::thread t1(foo);
     std::thread t2(foo);
+    // 늦게 도착한 넘은 대기!
+    // mutex 획득 못한 경우, 대기상태
 
     t1.join();
     t2.join();
 }
-
-
-
