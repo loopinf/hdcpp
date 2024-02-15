102ff390c0826032e321dd495a751f766fb9b336
loopinf <loopinf@gmail.com>
2024-02-15 13:14:27 +0900

    thread start

--- 32_THREAD_BASIC/1_this_thread2.cpp
+++ 32_THREAD_BASIC/1_this_thread2.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-using namespace std::literals;
+using namespace std::literals; // 3s, 3ns 등을위해, user defined literal
 
 int main()
 {
-    std::this_thread::sleep_for(3);
-}
+    // #1. sleep_for : 특정시간동안 스레드 재우기
+    // => 주의: 인자는 정수가 아님
+    //          chrono type으로 해야
+    // std::this_thread::sleep_for(3);
+    std::this_thread::sleep_for(std::chrono::seconds{3});
+    std::this_thread::sleep_for(3s);  // 3s : 3초객체
+    std::this_thread::sleep_for(3ms); // 3s : 3초객체
+    std::this_thread::sleep_for(3ns); // 3s : 3초객체
 
+    3.4;  // double
+    3.4f; // float
+}
