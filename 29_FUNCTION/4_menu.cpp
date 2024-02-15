77700810e78991d6d292da37358c32048d7bb72e
loopinf <loopinf@gmail.com>
2024-02-15 10:33:43 +0900

    template specialization

--- 29_FUNCTION/4_menu.cpp
+++ 29_FUNCTION/4_menu.cpp
@@ -7,29 +7,30 @@ using namespace std::placeholders;
 class BaseMenu
 {
 	std::string title;
+
 public:
-	BaseMenu(const std::string& title) : title{title} {}
+	BaseMenu(const std::string &title) : title{title} {}
 	virtual ~BaseMenu() {}
-	
+
 	std::string get_title() const { return title; }
 	virtual void command() = 0;
 };
 
-
 class PopupMenu : public BaseMenu
 {
-	std::vector< std::shared_ptr<BaseMenu> > v;  
-public:
-	PopupMenu(const std::string& title) : BaseMenu{title} {}
+	std::vector<std::shared_ptr<BaseMenu>> v;
 
-	void add(const std::shared_ptr<BaseMenu>& m) { v.push_back(m); }
+public:
+	PopupMenu(const std::string &title) : BaseMenu{title} {}
+
+	void add(const std::shared_ptr<BaseMenu> &m) { v.push_back(m); }
 
 	void command()
 	{
 		while (1)
 		{
-		//	system("clear"); // linux
-		//  system("cls")	 // windows
+			//	system("clear"); // linux
+			//  system("cls")	 // windows
 
 			auto sz = v.size();
 
@@ -50,7 +51,7 @@ public:
 			if (cmd < 1 || cmd > sz + 1)
 				continue;
 
-			v[cmd - 1]->command();	
+			v[cmd - 1]->command();
 		}
 	}
 };
@@ -62,19 +63,18 @@ class MenuItem : public BaseMenu
 	std::vector<HANDLER> v;
 
 public:
-	void add_handler(HANDLER h) { v.push_back(h);}
-	MenuItem(const std::string& title, int id, HANDLER h = nullptr) : BaseMenu{title}, id{id}  {}
+	void add_handler(HANDLER h) { v.push_back(h); }
+	MenuItem(const std::string &title, int id, HANDLER h = nullptr) : BaseMenu{title}, id{id} {}
 
 	void command()
 	{
 		// std::cout << get_title() << " 메뉴가 선택됨\n";
-		for (auto f:v)
-		f();
-
+		for (auto f : v)
+			f();
 	}
 };
 
-void f0() { std::cout << "f0\n";}
+void f0() { std::cout << "f0\n"; }
 
 int main()
 {
@@ -83,18 +83,15 @@ int main()
 	std::shared_ptr<PopupMenu> pm2 = std::make_shared<PopupMenu>("해상도 변경");
 
 	root->add(pm1);
-	root->add(pm2); 
+	root->add(pm2);
 
-	pm1->add(std::make_shared<MenuItem>("RED",   11, &f0));
+	pm1->add(std::make_shared<MenuItem>("RED", 11, &f0));
 	pm1->add(std::make_shared<MenuItem>("GREEN", 12));
-	pm1->add(std::make_shared<MenuItem>("BLUE",  13));
+	pm1->add(std::make_shared<MenuItem>("BLUE", 13));
 
-	pm2->add(std::make_shared<MenuItem>("HD",  21));
+	pm2->add(std::make_shared<MenuItem>("HD", 21));
 	pm2->add(std::make_shared<MenuItem>("FHD", 22));
 	pm2->add(std::make_shared<MenuItem>("UHD", 23));
 
 	root->command();
 }
-
-
-
--- 30_TEMPLATE_SPECIALIZATION/specialization1.cpp
+++ 30_TEMPLATE_SPECIALIZATION/specialization1.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 
+// primary template
 template<typename T>
 class MyContainer
 {
 public:
 	void push_back(const T& arg) { std::cout << "T\n"; }
+};
+// partial specialization ( 부분 특수화, 부분 전문화)
+template<typename T>
+class MyContainer<T*>
+{
+public:
+	void push_back(const T* arg) { std::cout << "T*\n"; }
+};
+
+// specialization 
+template<>
+class MyContainer<char*>
+{
+public:
+	void push_back(const char* arg) { std::cout << "char*\n"; }
 };
 
 int main()
