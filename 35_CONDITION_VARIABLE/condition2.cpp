17e5b772fc65bc63ecdaed8166f43b4ec41934fa
loopinf <loopinf@gmail.com>
2024-02-15 16:28:15 +0900

    .

--- 35_CONDITION_VARIABLE/condition2.cpp
+++ 35_CONDITION_VARIABLE/condition2.cpp
@@ -12,8 +12,6 @@ int shared_data = -1; // 아직 데이터 없음
 // 2. 전역변수 생성
 std::condition_variable cv;
 
-
-
 void consumer()
 {
     // 3. 소비자는 unique_lock으로 mutex 획득
@@ -26,21 +24,23 @@ void consumer()
                 // 3. 신호가 오면 다시, u.lock() 하고
                 // 4. 아랫줄 실행
 
-
     std::cout << "consume : " << shared_data << std::endl;
 }
 // 생산자, 쓰기 전용
 void producer()
 {
     std::this_thread::sleep_for(10ms);
-    std::lock_guard<std::mutex> lg(m);
-    shared_data = 100;
-    std::cout << "produce : " << shared_data << std::endl;
+    {
+        std::lock_guard<std::mutex> lg(m);
+        shared_data = 100;
+        std::cout << "produce : " << shared_data << std::endl;
+    }
+    cv.notify_one();
 }
 int main()
 {
     std::thread t1(producer);
     std::thread t2(consumer);
-    t1.join(); 
+    t1.join();
     t2.join();
 }
