cfe09ff8295cf7e60c261542bdbc50e50638b9a8
loopinf <loopinf@gmail.com>
2024-02-15 11:24:02 +0900

    template quiz

--- 30_TEMPLATE_SPECIALIZATION/traits3.cpp
+++ 30_TEMPLATE_SPECIALIZATION/traits3.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include <type_traits>
+
+// traits2.cpp 는 에러 아닌데, 
+// 아래코드는 컴파일 에러임
+// why?
 
 template<typename T> 
 void printv(const T& value)
 {
-	if ( T 가 포인터 타입 이면 )
+	if ( std::is_pointer<T>::value)
 		std::cout << value << " : " << *value << std::endl;
 	else 
 		std::cout << value << std::endl;
