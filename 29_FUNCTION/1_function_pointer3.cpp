01aedf51767654d61f9d2ef4e3e6938669a8a528
loopinf <loopinf@gmail.com>
2024-02-15 09:24:01 +0900

    function pointer invoke

--- 29_FUNCTION/1_function_pointer3.cpp
+++ 29_FUNCTION/1_function_pointer3.cpp
@@ -1,3 +1,4 @@
+#include <functional>
 // C 포인터의 단점 #2
 // => 일반 함수 포인터에 멤버함수의 주소를 담을 수 없음
 class Dialog 
@@ -10,7 +11,7 @@ void f1(int a) {}
 int main()
 {
 	// 1. 함수 포인터 모양 알아두자
-	void(*f1)(int) = &f1;   // & 생략가능, f1 으로도 가능하나, 암시적 형변환 되어서 그런것.
+	void(*f)(int) = &f1;   // & 생략가능, f1 으로도 가능하나, 암시적 형변환 되어서 그런것.
 	void(Dialog::*f2)(int) = &Dialog::close; // & 반드시 필요
 
 	// 2. 함수 포인터로 함수 호출하는 방법
@@ -18,4 +19,13 @@ int main()
 	
 	f1(10);			// non-member function pointer
 	(dlg.*f2)(10);  // member function pointer
+
+	// 3. 위 2개의 코드를 동일한 형식으로 호출할 수 없을까?
+	// => C++17 의 std::invoke
+	std::invoke(f1, 10); // f1(10)
+	std::invoke(f2, &dlg, 10); // (dlg.*f2)(10)
+
+	// 4. 
+	// std::invoke : 모든 함수 포인터를 "동일한 방식으로 호출"하는 도구
+	// std::function : 모든 함수 주소를 하나의 객체에 보관하는 도구
 }
