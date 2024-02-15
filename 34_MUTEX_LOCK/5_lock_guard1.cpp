8da355b6b448c649319fbd5f3c1330fb82488984
loopinf <loopinf@gmail.com>
2024-02-15 15:47:40 +0900

    .

--- 34_MUTEX_LOCK/5_lock_guard1.cpp
+++ 34_MUTEX_LOCK/5_lock_guard1.cpp
@@ -7,26 +7,27 @@ std::mutex m;
 //  C++ 격언 : 자원 반납은 함수 끝에서 하지마라
 //              생성자 / 소멸자에 의존해라. RAII
 
-
-template<typename T>
+template <typename T>
 struct lock_guard
 {
-    T& mtx;
-    lock_guard(T& m) : mtx{m} {m.lock();}
-    ~lock_guard()             {m.unlock();}
+    T &mtx;
+    lock_guard(T &m) : mtx{m} { m.lock(); }
+    ~lock_guard() { m.unlock(); }
 };
 
 void goo()
 {
-    lock_guard<std::mutex> g(m); // 생성자에서 m.lock() 
-                                    // 소멸자가 m.unlock()
-                                    // 예외 발생해도 g의 소멸자 호출됨
-    // m.lock();    
-    std::cout << "using shared data" << std::endl;
-    // throw 1;     
-    // m.unlock();
-}
 
+    {
+        lock_guard<std::mutex> g(m); // 생성자에서 m.lock()
+                                     // 소멸자가 m.unlock()
+                                     // 예외 발생해도 g의 소멸자 호출됨
+        // m.lock();
+        std::cout << "using shared data" << std::endl;
+        // throw 1;
+        // m.unlock();
+    }
+}
 
 void foo()
 {
@@ -40,6 +41,3 @@ int main()
     t1.join();
     t2.join();
 }
-
-
-
