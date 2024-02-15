15f89ea380c4b886e37e0bd986f0c70f7668b9b0
loopinf <loopinf@gmail.com>
2024-02-15 11:01:48 +0900

    template

--- 30_TEMPLATE_SPECIALIZATION/specialization4.cpp
+++ 30_TEMPLATE_SPECIALIZATION/specialization4.cpp
@@ -6,8 +6,8 @@ int main()
 	std::vector<int>  v1 = {1,2,3};
 	std::vector<bool> v2 = {1,0,1};
 
-	auto a1 = v1[0];
-	auto a2 = v2[0];
+	auto a1 = v1[0]; // int a1 = v1[0]
+	auto a2 = v2[0]; // bit_proxy a2 = v2[0]
 
 	std::cout << typeid(a1).name() << std::endl;
 	std::cout << typeid(a2).name() << std::endl;
--- 30_TEMPLATE_SPECIALIZATION/specialization5.cpp
+++ 30_TEMPLATE_SPECIALIZATION/specialization5.cpp
@@ -1,7 +1,9 @@
-
+// 아래 코드가 실제 C++표준에 있습니다. 
+// C++98 시절 만든 버전 
+template<typename T>
 struct plus 
 {
-	int operator()(int a, int b) const 
+	int operator()(T a, T b) const 
 	{
 		return a + b;
 	}
@@ -9,8 +11,9 @@ struct plus
 
 int main()
 {
-	plus p;
+	plus<int> p;
 
-	auto ret1 = p(1, 2);
-	auto ret2 = p(1, 2.1);
+	auto ret1 = p(1, 2);  // ok
+	auto ret2 = p(1, 2.1); // ?
+	
 }
