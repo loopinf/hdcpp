1bac470a73f6569f0d113106c00b758b6a1a12c5
loopinf <loopinf@gmail.com>
2024-02-15 13:28:42 +0900

    ..

--- 32_THREAD_BASIC/2_thread2.cpp
+++ 32_THREAD_BASIC/2_thread2.cpp
@@ -12,6 +12,13 @@ int main()
     std::string s = "hello";
 
      // 인자 전달 방법
+     std::thread t1(&f1); // 인자없는 함수 스레드로 실행
+     std::thread t2(&f2, 1, 3.4); // 스레드함수에 인자 전달
 
+     std::thread t3(&f3, 1, std::ref(n), // n을 reference로 보내라. but ref 보내는 건 위험한 코드임. 
+                    std::move(s));
+     t1.join();
+     t2.join();
+     t3.join();
 }
 
