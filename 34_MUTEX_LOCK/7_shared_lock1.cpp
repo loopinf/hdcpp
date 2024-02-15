1ec987fd38c15ab9542dcef34ca1ecfd52613c31
loopinf <loopinf@gmail.com>
2024-02-15 16:11:36 +0900

    .

--- 34_MUTEX_LOCK/7_shared_lock1.cpp
+++ 34_MUTEX_LOCK/7_shared_lock1.cpp
@@ -8,7 +8,7 @@
 using namespace std::literals;
 
 // 1. 읽는 동안 쓸 수 없다.
-// 2. 쓰는 동안 쓸 수 없다. 
+// 2. 쓰는 동안 쓸 수 없다.
 // 3. 쓰는 동안 읽을 수 없다.
 
 // 4. 읽는 동안 읽어도 된다!!
@@ -26,14 +26,14 @@ void Writer()
     while (1)
     {
         {
-
-        std::lock_guard<std::shared_mutex> g(m);
-        // m.lock();
-        share_data = share_data + 1;
-        std::cout << "Writer : " << share_data << std::endl;
-        std::this_thread::sleep_for(1s);
-        // m.unlock();
-        std::this_thread::sleep_for(10ms);
+            // Writer 는 lock() / unlock()
+            std::lock_guard<std::shared_mutex> g(m);
+            // m.lock();
+            share_data = share_data + 1;
+            std::cout << "Writer : " << share_data << std::endl;
+            std::this_thread::sleep_for(1s);
+            // m.unlock();
+            std::this_thread::sleep_for(10ms);
         }
     }
 }
@@ -41,11 +41,14 @@ void Reader(std::string_view name)
 {
     while (1)
     {
-        m.lock_shared();
-        std::cout << "Reader(" << name << ") : " << share_data << std::endl;
-        std::this_thread::sleep_for(500ms);
-        m.unlock_shared();
-        std::this_thread::sleep_for(10ms);
+        {
+            std::shared_lock<std::shared_mutex> g(m);
+            // m.lock_shared();
+            std::cout << "Reader(" << name << ") : " << share_data << std::endl;
+            std::this_thread::sleep_for(500ms);
+            // m.unlock_shared();
+            std::this_thread::sleep_for(10ms);
+        }
     }
 }
 
@@ -53,7 +56,7 @@ int main()
 {
     std::thread t1(Writer);
     std::thread t2(Reader, "A");
-    std::thread t3(Reader, "B");  // 읽는 동안에 다른애들도 읽을 수 있어야 함.
+    std::thread t3(Reader, "B"); // 읽는 동안에 다른애들도 읽을 수 있어야 함.
     std::thread t4(Reader, "C");
     t1.join();
     t2.join();
