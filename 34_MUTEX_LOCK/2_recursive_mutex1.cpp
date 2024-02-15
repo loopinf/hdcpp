fba0ee1b739961f8d54d5716604c245fc20a7f83
loopinf <loopinf@gmail.com>
2024-02-15 15:27:31 +0900

    .

--- 34_MUTEX_LOCK/2_recursive_mutex1.cpp
+++ 34_MUTEX_LOCK/2_recursive_mutex1.cpp
@@ -4,16 +4,19 @@
 #include <mutex>
 using namespace std::literals;
 
-std::mutex m;
+// std::mutex m;
+std::recursive_mutex m;
 int share_data = 0;
 
 void foo()
 {
     m.lock();
     m.lock();  // 소유자가 한번더?? lock 안됨.
+                // recursive 는 됨.
     share_data = 100;
     std::cout << "using share_data" << std::endl;
     m.unlock();
+    m.unlock();  // 두번 풀어야함
 }
 int main()
 {
