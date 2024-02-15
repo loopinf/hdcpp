9c2d3040b867b89e4efbc311a31e61dea1fabd23
loopinf <loopinf@gmail.com>
2024-02-15 13:31:47 +0900

    thread call 가능한거 모두. pthread랑 비교?

--- 32_THREAD_BASIC/2_thread3.cpp
+++ 32_THREAD_BASIC/2_thread3.cpp
@@ -15,5 +15,18 @@ struct Work
 
 int main()
 {
+    std::thread t1(&foo, 1, 3.4);
+
+    Machine m;
+    std::thread t2(&Machine::Run, &m, 1, 3.4);
+
+    Work w;
+    std::thread t3(w, 1, 3.4); //주의 &w가 아닌 w
+                                // w(1,3.4) 를 thread로
+
+    t1.join();
+    t2.join();
+    t3.join();
+
 }
 
