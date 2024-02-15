5ccbb26d20d307f922050c7a64c0c8000d03e45a
loopinf <loopinf@gmail.com>
2024-02-15 13:58:33 +0900

    vector에 thread 담아라

--- 32_THREAD_BASIC/2_thread5.cpp
+++ 32_THREAD_BASIC/2_thread5.cpp
@@ -8,13 +8,18 @@ void do_work(unsigned id) {}
 int main()
 {
     // 핵심 1. 스레드를 만들면 반드시 join(), detach() 해야 합니다.
+    // => "t1" 이라는 객체의 이름을 알고 있어야 함
     std::thread t1(&do_work, 1);
     t1.join();
 
     // 핵심 2. 10개의 스레드를 생성해 봅시다.
-
-	for( int i = 0; i < 10; i++)
-	{
-		
-	}
+    std::vector<std::thread> v(10); // thread obj 10 created
+                                    // thread 는 생성안됨
+    for (int i = 0; i < 10; i++)
+    {
+        v[i] = std::thread(&do_work, i);
+    }
+    // for (auto e : v)  // thread 객체를 복사하는 것임. ERROR
+    for (auto &e : v)
+        e.join();
 }
