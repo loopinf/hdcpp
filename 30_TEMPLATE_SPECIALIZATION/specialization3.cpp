92a7f652a4987e861f56e41a7a7c5ef6a9cd4362
loopinf <loopinf@gmail.com>
2024-02-15 10:46:25 +0900

    temp proxy 1

--- 30_TEMPLATE_SPECIALIZATION/specialization3.cpp
+++ 30_TEMPLATE_SPECIALIZATION/specialization3.cpp
@@ -17,8 +17,8 @@ public:
 
 struct bit_proxy
 {
-	int* ptr;
-	std::size_t idx;
+	int* ptr;        // 버퍼의 주소
+	std::size_t idx; // 접근할 인덱스 (비트단위)
 
 	bit_proxy(int* p, std::size_t idx) : ptr{p}, idx{idx} {}
 
@@ -44,11 +44,16 @@ class vector<bool>
 public:
 	vector(std::size_t size) : sz{size}
 	{
-		ptr = new int[(sz/32) + 1];
+		ptr = new int[(sz/32) + 1]; // sz 100 -> new int[4] 
 	}
 	~vector() { delete[] ptr; }
 
+	// 아래처럼 하면 byte (8bit)단위로 메모리 접근하게 됨.
+	// bool& operator[](int idx) { return ptr[idx]; } // T 대신 bool 쓰면  byte 단위로 리턴됨 
+	//  이럴때 사용하는 기술 "temporary proxy" 
+	// => 널리 알려진 기술임
 	bit_proxy operator[](std::size_t idx) { return bit_proxy{ptr, idx}; }
+
 };
 
 int main()
